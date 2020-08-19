b29f8c34dc60bf021cd4eea99b8a984fcc7bc56f
eljoedeleon <deliseo316@gmail.com>
2020-08-20 02:14:53 +0800

    Updates

---
+++ precache-manifest.f7707f021cbbfe0577df05329a172a3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d71aadd0d23cef719db160825dd89577",
+    "url": "/RandomQuoteMachine/index.html"
+  },
+  {
+    "revision": "5328788f68cf1bb5ee43",
+    "url": "/RandomQuoteMachine/static/css/main.8cd2a815.chunk.css"
+  },
+  {
+    "revision": "9b4206bab39c8a8b636d",
+    "url": "/RandomQuoteMachine/static/js/2.ec95844a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RandomQuoteMachine/static/js/2.ec95844a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5328788f68cf1bb5ee43",
+    "url": "/RandomQuoteMachine/static/js/main.cfd1096c.chunk.js"
+  },
+  {
+    "revision": "94ada78df0386eb4e8b4",
+    "url": "/RandomQuoteMachine/static/js/runtime-main.3c1c676d.js"
+  }
+]);
